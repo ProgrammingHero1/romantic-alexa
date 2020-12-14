2dd4afcf97919c924594ede2ad403401fca0420f
Pabitra Ghosh <72495034+Pabitra145@users.noreply.github.com>
2020-12-14 23:54:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,45 +16,51 @@ def talk(text):
     engine.runAndWait()
 
 
-def take_command():
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
     try:
-        with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'alexa' in command:
-                command = command.replace('alexa', '')
-                print(command)
-    except:
-        pass
+        print("Recognizing...")    
+        command = r.recognize_google(audio, language='en-in')
+        print(f"User said: {command}\n")
+
+    except Exception as e:
+            
+        print("Say that again please...")  
+        return "None"
     return command
 
 
-def run_alexa():
-    command = take_command()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
-    elif 'who the heck is' in command:
-        person = command.replace('who the heck is', '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-    elif 'date' in command:
-        talk('sorry, I have a headache')
-    elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
-    else:
-        talk('Please say the command again.')
+if __name__ == "__main__":
+    
+    while True:
+    
+        command = takecommand().lower()
+
+        if 'play' in command:
+            song = command.replace('play', '')
+            talk('playing ' + song)
+            pywhatkit.playonyt(song)
+        elif 'time' in command:
+            time = datetime.datetime.now().strftime('%I:%M %p')
+            talk('Current time is ' + time)
+        elif 'who the heck is' in command:
+            person = command.replace('who the heck is', '')
+            info = wikipedia.summary(person, 1)
+            print(info)
+            talk(info)
+        elif 'date' in command:
+            talk('sorry, I have a headache')
+        elif 'are you single' in command:
+            talk('I am in a relationship with wifi')
+        elif 'joke' in command:
+            talk(pyjokes.get_joke())
+        else:
+            talk('Please say the command again.')
 
 
-while True:
-    run_alexa()
+
