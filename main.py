5f46ba5547d23819757bc5a93f1612c9d99d47d2
Romeo Kienzler <romeo.kienzler@gmail.com>
2021-01-08 09:29:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,17 +17,14 @@ def talk(text):
 
 
 def take_command():
-    try:
-        with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'alexa' in command:
-                command = command.replace('alexa', '')
-                print(command)
-    except:
-        pass
+    with sr.Microphone() as source:
+        print('listening...')
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        command = command.lower()
+        if 'alexa' in command:
+            command = command.replace('alexa', '')
+            print(command)
     return command
 
 
