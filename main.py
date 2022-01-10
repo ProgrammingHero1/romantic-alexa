fb5422da2d96e1280e9c81fe0e6ae65a54f720af
Shahzaib Ur Rehman <60410166+shahzaibshazoo@users.noreply.github.com>
2022-01-11 00:52:51 +0500

    Update main.py
    to solve local variable 'command' referenced before assignment error

--- main.py
+++ main.py
@@ -26,35 +26,32 @@ def take_command():
             if 'alexa' in command:
                 command = command.replace('alexa', '')
                 print(command)
-    except:
-        pass
-    return command
+                    if 'play' in command:
+                        song = command.replace('play', '')
+                        talk('playing ' + song)
+                        pywhatkit.playonyt(song)
+                    elif 'time' in command:
+                        time = datetime.datetime.now().strftime('%I:%M %p')
+                        talk('Current time is ' + time)
+                    elif 'who the heck is' in command:
+                        person = command.replace('who the heck is', '')
+                        info = wikipedia.summary(person, 1)
+                        print(info)
+                        talk(info)
+                    elif 'date' in command:
+                        talk('sorry, I have a headache')
+                    elif 'are you single' in command:
+                        talk('I am in a relationship with wifi')
+                    elif 'joke' in command:
+                        talk(pyjokes.get_joke())
+                    else:
+                        talk('Please say the command again.')
+                    except:
+                        pass
 
 
-def run_alexa():
-    command = take_command()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
-    elif 'who the heck is' in command:
-        person = command.replace('who the heck is', '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-    elif 'date' in command:
-        talk('sorry, I have a headache')
-    elif 'are you single' in command:
-        talk('I am in a relationship with wifi')
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
-    else:
-        talk('Please say the command again.')
+
 
 
 while True:
-    run_alexa()
+    take_command()
